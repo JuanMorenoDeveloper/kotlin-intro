2c43de80806383033275eeefa9cc6cad535725d6
Juan M.vi <earth001@gmail.com>
2018-06-20 23:54:48 -0300

    Se quita definición explicita de lista

--- src/main/kotlin/uy/edu/ude/app/MainKotlin.kt
+++ src/main/kotlin/uy/edu/ude/app/MainKotlin.kt
@@ -4,7 +4,7 @@ data class EstudianteKotlin(val nombre: String,
                             val puntaje: Int? = null)
 
 fun main(args: Array<String>) {
-  val estudiantes: List<EstudianteKotlin> = listOf(EstudianteKotlin("Julia"),
+  val estudiantes = listOf(EstudianteKotlin("Julia"),
       EstudianteKotlin("Marco", puntaje = 12))
   val mayorPuntaje = estudiantes.maxBy { it.puntaje ?: 0 }
   println("El mayor puntaje en Kotlin es de: $mayorPuntaje")
